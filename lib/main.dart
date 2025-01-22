92baa132336ccba3f104f68660bb9e42da1460a3
dev-yamamon <dev.yamamon01@gmail.com>
2025-01-22 23:42:20 +0900

    入室画面UIの実装

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'チャットアプリ'),
+      home: const MyHomePage(title: 'MyChat'),
     );
   }
 }
@@ -32,9 +32,84 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  String name='';
+  String room='';
+
+  void showError(String message){}
+  void enter(){}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title:Text(widget.title)),
+      body: ListView(
+        children: [
+          ListTile(
+            title: TextField(
+              decoration: InputDecoration(
+                hintText: 'room-1',
+                labelText: '部屋名', // ラベルとして表示するテキスト
+                border: OutlineInputBorder(), // 枠線を追加
+              ),
+              onChanged: (value) {
+                this.room = value;
+              },
+            ),
+          ),
+          ListTile(
+            title: TextField(
+                decoration: InputDecoration(
+                  hintText: 'ヤマモン',
+                  labelText: 'あなたの名前', // ラベルとして表示するテキスト
+                  border: OutlineInputBorder(), // 枠線を追加
+                ),
+                onChanged: (value) {
+                  this.name = value;
+                }),
+          ),
+          ListTile(
+            title:ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+               backgroundColor: Colors.green,
+                iconColor: Colors.white,
+              ),
+                onPressed: enter,
+              icon: Image.asset(
+                'assets/enter.png',
+                width: 30, // アイコンの幅
+                height: 30,
+              ), // アイコンの高さ),
+                label: Text(
+                  '入室する',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight:FontWeight.bold,
+                    fontSize: 20
+                  ),
+                ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
+class ChatPage extends StatefulWidget {
+  ChatPage({required this.name,required this.room,super.key});
+  String name;
+  String room;
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text(widget.room)),
+    );
+  }
+}
+
