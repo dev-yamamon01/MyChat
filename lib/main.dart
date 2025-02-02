0f7ce629516593d7af8eba4151a84713d4a08dcb
dev-yamamon <dev.yamamon01@gmail.com>
2025-02-03 00:30:58 +0900

    チャットページのUIと一部関数の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
-void main() {
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
@@ -128,6 +141,52 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<Item> items=[];
+  late FirebaseFirestore firestore;
+  late CollectionReference<Map<String,dynamic>> collection;
+  final TextEditingController textEditingController=TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    firestore=FirebaseFirestore.instance;
+    collection=
+        firestore.collection('room').doc(widget.room).collection('items');
+    watch();
+  }
+
+  //データ更新監視
+  Future<void> watch () async{
+    collection.snapshots().listen((event){
+      if(mounted){
+        setState(() {
+            items=event.docs.reversed.map(
+            (docment)=>
+            Item.fromSnapshot(
+            docment.id,
+            docment.data(),
+            ),
+            )
+  .toList(growable: false);
+        });
+
+  }
+  }
+      );
+  }
+
+  //保存する
+  Future<void> save() async{
+    final now=DateTime.now();
+    await collection.doc(now.microsecondsSinceEpoch.toString()).set(
+{
+  "data":Timestamp.fromDate(now),
+  "name":widget.name,
+  "text":textEditingController.text,
+});
+    textEditingController.text='';
+  }
 
   send(){
     print("押されました");
@@ -137,40 +196,74 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title:Text(widget.room)),
-      body: ListView(
-            children: [
-              ListTile(
-                title: Text("test"),
-              )
-              ,Column(
+      body: Column(
                 children: [
-                  TextField(
-
+                  Expanded(
+                      child: ListView.builder(
+                  reverse: true,
+                  itemBuilder: (context,index){
+                  final item=items[index];
+                  return ListTile(
+                      subtitle: Text(item.name),
+                      title: Text(item.text),
+                      );
+                  },
+                  itemCount: items.length,)
                   ),
-                  ElevatedButton.icon(
+                  SafeArea(child:
+                  ListTile(
+                    title: TextField(
+                    ),
+                    trailing: ElevatedButton.icon(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      iconColor: Colors.white,
+                    backgroundColor: Colors.green,
+                    iconColor: Colors.white,
                     ),
                     onPressed: send,
                     icon: SvgPicture.asset('assets/send.svg',
-                      width: 20,
-                      height: 20),
+                    width: 20,
+                    height: 20),
 
-                  label: Text(
-                      '送信',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight:FontWeight.bold,
-                          fontSize: 20
-                      ),
+                    label: Text(
+                    '送信',
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontWeight:FontWeight.bold,
+                    fontSize: 20
                     ),
-                  ),
+                    ),
+                    ),
+                  ))
+
+
                 ],
-              )
-            ],
-          )
+              ),
     );
+
   }
+
 }
+
+        class Item{
+        const Item({
+        required this.id,
+        required this.name,
+        required this.text,
+        });
+
+        final String id;
+        final String name;
+        final String text;
+
+        factory Item.fromSnapshot(String id, Map<String,dynamic> document){
+        return Item(
+        id: id,
+        name: document['name'].toString() ?? '',
+        text: document['text'].toString() ?? '',
+        );
+
+        }
+        }
+
+
 
